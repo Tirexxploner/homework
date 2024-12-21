876c9b7846ba775dbd7aacca159b538f90a19035
Tirexxploner <krinsman67@gmail.com>
2024-12-21 03:42:49 +0300

    Add files via upload

---
+++ java80/js/main.js
@@ -0,0 +1,23 @@
+
+// for (let i = 2; i <= 12; i = i + 2) {
+//   console.log(i)
+// }
+
+
+// for (let x = 10; x >= 1; x = x - 1) {
+//   console.log(x)
+// }
+
+let i = 0
+let x = 11
+
+while(i <= 10){
+  i = i + 2
+  console.log(i)
+}
+
+while(x >= 2){
+  x = x - 1
+  console.log(x)
+}
+
